cf844b2a88eba791a2491bab0b6e3690487b1c88
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-04-24 14:32:33 -0500

    formulario de actividad

--- src/app/components/actividad/actividad.component.ts
+++ src/app/components/actividad/actividad.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+// import Swal from 'sweetalert2';
+import { ActividadesService } from '../../services/actividades.service';
 
 @Component({
   selector: 'app-actividad',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActividadComponent implements OnInit {
 
-  constructor() { }
+  // variables de ngmodel
+  Nombre: string;
+  FechaCreacion: Date;
+  FechaLimite: Date;
+  Estado: number;
+  Responsable: number;
+  actividades: any = [];
+
+  constructor(
+    private actividadesService: ActividadesService,
+  ) {
+    this.actividadesService.get('actividades/?limit=0')
+        .subscribe(res => {
+          if (res !== null) {
+            // console.log(res);
+            this.actividades = res;
+            console.log(this.actividades);
+          }
+        },
+        (error: HttpErrorResponse) => {
+          console.log('error en la peticion');
+          console.log(error);
+        });
+  }
 
   ngOnInit() {
+  }
+
+  agregarResponsable() {
+    const actividad = {
+      Nombre: this.Nombre,
+      FechaCreacion: this.FechaCreacion,
+      FechaLimite: this.FechaLimite,
+      Estado: this.Estado,
+      Responsable: this.Responsable
+    };
+    console.log(actividad);
+        this.actividadesService.post('actividades', actividad)
+        .subscribe(res => {
+          if (res !== null) {
+            console.log(res);
+          }
+        },
+        (error: HttpErrorResponse) => {
+          console.log('error en la peticion');
+          console.log(error);
+        });
   }
 
 }
