a4536d3c8b7340c518870e9e58a43e86d5a55057
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-04-24 12:58:15 -0500

    creacion inicial de servicio y obtencion de datos del formulario

--- src/app/components/resposable/resposable.component.ts
+++ src/app/components/resposable/resposable.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResposableComponent implements OnInit {
 
+  // variables de ngmodel
+  nombres: string;
+  apellidos: string;
+  identificacion: number;
+  email: string;
+  telefono: number;
+
+  // responsable = {
+  //   nombres: this.nombres,
+  //   apellidos: this.apellidos,
+  //   identificacion: this.identificacion,
+  //   email: this.email,
+  //   telefono: this.telefono
+  // };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  agregarResponsable() {
+    const responsable = {
+      nombres: this.nombres,
+      apellidos: this.apellidos,
+      identificacion: this.identificacion,
+      email: this.email,
+      telefono: this.telefono
+    };
+    console.log(responsable);
+  }
 }
